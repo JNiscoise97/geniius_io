e8a761dd02c45cd9355c58cf3d9b8e42d4e8e28e
jordan.niscoise <jordan.bluker@gmail.com>
2026-03-21 17:20:59 +0100

    [CONNECT] allow test

--- apps/connect/src/lib/completion/sectionCompletion.ts
+++ apps/connect/src/lib/completion/sectionCompletion.ts
@@ -24,7 +24,8 @@ const FAMILY_TREE_EARLY_ACCESS_PARTICIPANT_IDS = new Set<string>([
   "2c11debf-d475-48c4-95b5-01313ade4710",
   "0c014a55-3bd2-4022-8bf9-6eb4bcbbe28b",
   "df38d26b-db3b-4eb2-af95-3cf2c3bf2ea1",
-  "cee2967a-633a-4c14-88d4-5020281e41a1"
+  "cee2967a-633a-4c14-88d4-5020281e41a1",
+  "2c8eb65d-d0f2-45ef-9fd3-11d947554a1e"
 ]);
 
 export type CompletionItemType = "form" | "info";
